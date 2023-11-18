9f55fe077461cfa198059bf896561b0566ee1db9
Sougata Ghosh <sougataghosh314@gmail.com>
2023-11-18 20:04:43 +0530

    enums and route params

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -21,3 +22,25 @@ async def put_route():
 @app.delete("/")
 async def delete_route():
     return {"message": "you just hit a delete request"}
+
+
+class DayEnum(str, Enum):
+    MONDAY = "Monday"
+    TUESDAY = "Tuesday"
+    SUNDAY = "Sunday"
+
+
+@app.get("/days/{day_name}")
+async def get_day(day_name: DayEnum):
+    if day_name == DayEnum.TUESDAY:
+        return {
+            "day_name": day_name.name,
+            "day_value": day_name.value
+        }
+
+    if day_name.value == "Monday":
+        return {
+            "day_name": day_name,
+            "message": "you are on Monday",
+        }
+    return {"day_name": day_name, "message": "this is a different day"}
